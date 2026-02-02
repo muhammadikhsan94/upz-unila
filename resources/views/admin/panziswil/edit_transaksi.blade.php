e74658638c8ade8868654ee76b24de6ebeb0c9b6
muhammadikhsan94 <muhammadikhsan208@gmail.com>
2026-02-02 19:00:45 +0700

    update

--- resources/views/admin/panziswil/edit_transaksi.blade.php
+++ resources/views/admin/panziswil/edit_transaksi.blade.php
@@ -675,7 +675,7 @@
                         var html = '';
                         alert("Data berhasil disimpan!")
                         html = '<div class="alert alert-default">' + data + '</div>';
-                        window.location.replace("{{url('transaksi')}}");
+                        window.location.replace("{{route('panziswil.transaksi')}}");
                     },
                     error: function (data) {
                         var html = '';
